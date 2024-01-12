8b7bcead409aba6c19651e937e8950669ccfdf57
Avinash <avinashpottipadu7@gmnail.com>
2024-01-12 19:15:39 +0530

    ChromeDriver Script Executed

---
+++ src/test/java/basicprograms/LaunchBrowser.java
@@ -0,0 +1,32 @@
+package basicprograms;
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.testng.annotations.Test;
+
+public class LaunchBrowser {
+	
+	@Test
+	public void lanchBrowser() {
+		WebDriverManager.chromedriver().setup();
+		WebDriver driver = new ChromeDriver(); 
+		
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		
+		String actTitle="OrangeHRM";
+		String expTitle= driver.getTitle();
+		
+		Assert.assertEquals(expTitle, actTitle, "Title As Expected");
+		
+		driver.close();
+		
+		
+	}
+
+}
