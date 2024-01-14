2e56b919b4dcf9f33e115dd56a71a212d71c2cd9
Avinash <avinashpottipadu7@gmnail.com>
2024-01-14 16:17:49 +0530

    14 Jan 2024

---
+++ src/test/java/basicprograms/LaunchFireFoxBrowser.java
@@ -0,0 +1,26 @@
+package basicprograms;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LaunchFireFoxBrowser {
+
+	@Test
+	public void lanchFireFoxBrowser() {
+		WebDriverManager.firefoxdriver().setup();
+		WebDriver driver = new FirefoxDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		String actTitle="OrangeHRM";
+		String expTitle= driver.getTitle();
+		
+		Assert.assertEquals(expTitle, actTitle, "Title As Expected");
+		
+		driver.close();
+		
+	}
+}
---
+++ src/test/java/basicprograms/LaunchSafariBrowser.java
@@ -0,0 +1,25 @@
+package basicprograms;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.safari.SafariDriver;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class LaunchSafariBrowser {
+
+	@Test
+	public void lanchSafariBrowser() {
+		WebDriverManager.safaridriver().setup();
+		WebDriver driver = new SafariDriver();
+		driver.manage().deleteAllCookies();
+		driver.manage().window().maximize();
+		driver.get("https://opensource-demo.orangehrmlive.com/web/index.php/auth/login");
+		String actTitle="OrangeHRM";
+		String expTitle= driver.getTitle();
+		
+		Assert.assertEquals(expTitle, actTitle, "Title As Expected");
+		
+		driver.close();
+	}
+}
